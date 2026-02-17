65d50f0b6870ac144e821923ae1810a5ade6fa89
Elsa Minsut <elsaminsut@gmail.com>
2026-02-17 18:42:11 +0100

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -9,18 +9,19 @@ animals_data = load_data('animals_data.json')
 
 output = ""
 for animal in animals_data:
+    output += '<li class="cards__item">'
     if animal.get("name"):
-        output += f"Name: {animal.get("name")}\n"
+        output += f"Name: {animal.get("name")}<br/>\n"
     if animal["characteristics"].get("diet"):
-        output += f"Diet: {animal["characteristics"].get("diet")}\n"
+        output += f"Diet: {animal["characteristics"].get("diet")}<br/>\n"
     if animal.get("locations"):
-        output += f"Location: {animal.get("locations")[0]}\n"
+        output += f"Location: {animal.get("locations")[0]}<br/>\n"
     if animal["characteristics"].get("type"):
-        output += f"Type: {animal["characteristics"]["type"]}\n\n"
+        output += f"Type: {animal["characteristics"]["type"]}<br/>\n\n"
+    output += '</li>'
 
 html_file = open("animals_template.html", "r", encoding="utf-8")
 source_code = html_file.read()
 source_code_with_animals = source_code.replace("__REPLACE_ANIMALS_INFO__", output)
 with open("animals.html", "w") as f:
   f.write(source_code_with_animals)
-  
